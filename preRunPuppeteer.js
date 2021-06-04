5ef927201f0cd65019cf7d16761c2e3e211b1f7c
thisnamewasnottaken <8645373+thisnamewasnottaken@users.noreply.github.com>
2021-06-04 18:07:23 +0100

    Managed to unbreak it...

--- preRunPuppeteer.js
+++ preRunPuppeteer.js
@@ -1,27 +1,34 @@
 // https://dev.to/checkly/setting-state-using-cookies-1bk
 
-
-
+console.log("[INFO][preRunPuppeteer] Entering");
 module.exports = async (browser, context) => {
-  // this is not a real login scenario. 
-  const page = await browser.newPage();
+  // this is not a real login scenario... we need some constants 
+  const {cookiesource} = require('./constantsSiteDetails');
+  const puppeteer = require('puppeteer');
+  const fs = require('fs');
 
-  await page.goto(sitedetails.cookiesource.cookieurl);
-  await page.close();
-}
-
-
-const puppeteer = require('puppeteer')
-const fs = require('fs')
-const sitedetails = require('./constantsSiteDetails.js')
-
-(async () => {
-  const browser = await puppeteer.launch()
+  // Go to a new page.
   const page = await browser.newPage()
 
-  await page.goto('https://github.com/login')
+  // Read in the cookie values from cookie.json in this folder.
+  const cookies = fs.readFileSync('cookie.json', 'utf8')
+    // console.log("[INFO][preRunPuppeteer] Reading cookie! ");
+    // console.log(typeof cookies);
 
-  await page.waitForNavigation()
+  const deserializedCookies = JSON.parse(cookies)
+  // TODO: Error handling for when cookie isn't there...
+    // console.log("[INFO][preRunPuppeteer] Checkout desearalized stuff types! ");
+    // console.log(typeof deserializedCookies);  
+    // console.log("[INFO][preRunPuppeteer] Checkout desearalized stuff values! ");
+    // console.log(deserializedCookies)
 
-  await page.close()
-})()
+  // Set the cookies
+  await page.setCookie(...deserializedCookies)
+
+  // Go to the page to validate (useful in when debugging in headfull mode)
+  await page.goto(cookiesource.preRunPuppeteerlink);
+  // TODO: Check page loads with 200
+
+  await page.close();
+  console.log("[INFO][preRunPuppeteer] Exiting");
+};
