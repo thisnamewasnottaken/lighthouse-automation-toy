d829a72b850de7038d0992c67289d0a0d53eebbb
thisnamewasnottaken <8645373+thisnamewasnottaken@users.noreply.github.com>
2021-06-04 21:20:51 +0100

    Added 3 core passes.

--- lighthouseConfig.js
+++ lighthouseConfig.js
@@ -20,55 +20,45 @@ const {cookiesource} = require('./constantsSiteDetails');
         ]	    
   },
  
-   // 2. Add gatherer to the default Lighthouse load ('pass') of the page.
-  //  passes: [
-  //     {
-  //       passName: 'desktopUnthrottledPass',
-  //       recordTrace: true,
-  //       useThrottling: false,
-  //       pauseAfterFcpMs: 1000,
-  //       pauseAfterLoadMs: 1000,
-  //       networkQuietThresholdMs: 1000,
-  //       cpuQuietThresholdMs: 1000,
-  //       gatherers: [],
-  //       formFactor: 'desktop',
-  //       screenEmulation: constants.screenEmulationMetrics.desktop,
-  //       emulatedUserAgent: constants.userAgents.desktop,
-  //     },
-  //     {
-  //       passName: 'desktopThrottledPass',
-  //       recordTrace: true,
-  //       useThrottling: true,
-  //       pauseAfterFcpMs: 1000,
-  //       pauseAfterLoadMs: 1000,
-  //       networkQuietThresholdMs: 1000,
-  //       cpuQuietThresholdMs: 1000,
-  //       gatherers: [],
-  //       formFactor: 'desktop',
-  //       throttling: constants.throttling.desktopDense4G,
-  //       screenEmulation: constants.screenEmulationMetrics.desktop,
-  //       emulatedUserAgent: constants.userAgents.desktop,
-  //     },
-  //     {
-  //       passName: 'defaultPass',
-  //       recordTrace: true,
-  //       useThrottling: true,
-  //       pauseAfterFcpMs: 1000,
-  //       pauseAfterLoadMs: 1000,
-  //       networkQuietThresholdMs: 1000,
-  //       cpuQuietThresholdMs: 1000,
-  //       gatherers: [],
-  //     },],
- 
-   // 4. Create a new 'My site metrics' section in the default report for our results.
-  //  categories: {
-  //    mysite: {
-  //      title: 'My site metrics is sic!',
-  //      description: 'Metrics for our super duper awesome site',
-  //      auditRefs: [
-  //        // When we add more custom audits, `weight` controls how they're averaged together.
-  //        //{id: 'searchable-audit', weight: 1},
-  //      ],
-  //    },
-  //  },
+  //2. Add gatherer to the default Lighthouse load ('pass') of the page.
+   passes: [
+    {
+      passName: 'defaultPass',
+      recordTrace: true,
+      useThrottling: true,
+      pauseAfterFcpMs: 1000,
+      pauseAfterLoadMs: 1000,
+      networkQuietThresholdMs: 1000,
+      cpuQuietThresholdMs: 1000,
+      gatherers: [],
+    },
+    {
+    passName: 'desktopUnthrottledPass',
+    recordTrace: true,
+    useThrottling: false,
+    pauseAfterFcpMs: 1000,
+    pauseAfterLoadMs: 1000,
+    networkQuietThresholdMs: 1000,
+    cpuQuietThresholdMs: 1000,
+    gatherers: [],
+    formFactor: 'desktop',
+    screenEmulation: constants.screenEmulationMetrics.desktop,
+    emulatedUserAgent: constants.userAgents.desktop,
+    },
+    // {
+    //   passName: 'desktopThrottledPass',
+    //   recordTrace: true,
+    //   useThrottling: true,
+    //   pauseAfterFcpMs: 1000,
+    //   pauseAfterLoadMs: 1000,
+    //   networkQuietThresholdMs: 1000,
+    //   cpuQuietThresholdMs: 1000,
+    //   gatherers: [],
+    //   formFactor: 'desktop',
+    //   throttling: constants.throttling.desktopDense4G,
+    //   screenEmulation: constants.screenEmulationMetrics.desktop,
+    //   emulatedUserAgent: constants.userAgents.desktop,
+    // },
+
+    ],
  };
